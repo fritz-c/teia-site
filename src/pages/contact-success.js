807c9cc92048e7fb4e23db926d6c702c4b2dd7e6
fritz-c <fritz-c@users.noreply.github.com>
2018-02-17 17:50:10 +0900

    feat: make rudimentary contact form

---
+++ src/pages/contact-success.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+const ContactSuccessPage = () => (
+  <div>
+    Thank you for contacting us! We&quot;ll be back with you shortly.<br />
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+);
+
+export default ContactSuccessPage;
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -5,6 +5,17 @@ const SecondPage = () => (
   <div>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <form data-netlify name="contact" action="/contact-success/">
+      <label htmlFor="email">
+        Your Email: <input type="email" name="email" />
+      </label>
+      <br />
+      <label htmlFor="message">
+        Message: <input type="text" name="message" />
+      </label>
+      <br />
+      <button type="submit">Send!</button>
+    </form>
     <Link to="/">Go back to the homepage</Link>
   </div>
 );
